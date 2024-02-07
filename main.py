89d4f945eda78317418ff45fbbc7924fb3dba6c2
Disha Devani <ddevani@dc.nexuslink.com>
2024-02-07 10:36:12 +0530

    fastAPI

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Request,HTTPException
+from fastapi import FastAPI, Request,HTTPException,Form 
 from enum import Enum
+from pydantic import BaseModel
+
 
 app=FastAPI()
 
@@ -9,7 +11,7 @@ async def greetings():
     return ("Greetings from myside ")
 
 
-# ---------------calculator----------
+# ---------------calculator----------(using selection )
 class ModelName(str, Enum):
     addition = "+"
     subtraction = "-"
@@ -27,17 +29,20 @@ def get_model(operator: ModelName, x: int, y: int):
     elif operator == ModelName.division:
         return {"result": x / y}
 
-# @app.get("/calculator/{x}/{y}")
-# def calc(x: int, y: int, operation: str, operator: str):
-#     if operation.lower() == "addition" and operator == "+":
-#         return x + y
-#     if operation.lower() == "substraction" and operator == "-":
-#         return x - y
-#     if operation.lower() == "multiplication" and operator == "*":
-#         return x * y
-#     if operation.lower() == "division" and operator == "/":
-#         return x / y
-#     return "Invalid operation or operator"
+
+
+# ----------------calculator(manual)
+@app.get("/calculator/{x}/{y}")
+def calc(x: int, y: int, operation: str, operator: str):
+    if operation.lower() == "addition" and operator == "+":
+        return x + y
+    if operation.lower() == "substraction" and operator == "-":
+        return x - y
+    if operation.lower() == "multiplication" and operator == "*":
+        return x * y
+    if operation.lower() == "division" and operator == "/":
+        return x / y
+    return "Invalid operation or operator"
 
 
 # -------------taking inputs and displaying
@@ -46,6 +51,14 @@ def details(name:str,number:int):
     return {"name":name,"Mobile Number": number}
 
 
+# -----------defining scema and use of forms in fastAPI
+
+class schema(BaseModel):
+    username:str
+    Mobile_Number:int
+
 @app.post("/form/data")
-def form_data(username:str):
-    return {"username":username}
+def form_data(data:schema):
+    return {"username":data.username,"Mobile_Number":data.Mobile_Number}
+
+# -------------
